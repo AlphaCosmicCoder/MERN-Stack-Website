c8448c84c4c2a17894be193ba9f731b823555cd5
Ayush Kumar Singh <83543768+099ayuh@users.noreply.github.com>
2022-09-08 19:57:12 +0530

    Adding my new Website

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/e-com-website', {
+const db = 'mongodb+srv://Ayush:mongo1234@cluster0.gbiaesu.mongodb.net/e-com-website?retryWrites=true&w=majority';
+mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
